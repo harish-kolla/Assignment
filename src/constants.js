ac37f43ccd0558f89da45881d07465da20ff3e21
harish-kolla <harishkolla349@gmail.com>
2022-09-10 14:45:21 -0500

    moved all the static text to constants file

---
+++ src/constants.js
@@ -0,0 +1,11 @@
+export const staticKeys = {
+  remove: "Remove",
+  item: "Item",
+  itemDesc: "Item Description",
+  price: "Price",
+  delete: "Delete",
+  loadMore: "Loading 50 more records...",
+  topPage: "Top Page",
+  addRow: "Add Row",
+  virtualizedList: "Virtualized List",
+};
